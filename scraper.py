516cf884a85055c8dc326c7644ab8ca280bfb5bc
nicholasctruong <cole.c1519@gmail.com>
2022-10-28 15:42:38 -0700

    temporary print

--- scraper.py
+++ scraper.py
@@ -19,7 +19,9 @@ def extract_next_links(url, resp):
     
     if resp.status == 200:
         soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
-        return [link.get('href') for link in soup.find_all('a')]
+        print([link.get('href') for link in soup.find_all('a')])
+        return []
+        # return [link.get('href') for link in soup.find_all('a')]
     else:
         return []
 
