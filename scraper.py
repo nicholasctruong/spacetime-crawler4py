b9a5a1cc6d613d4460eb21da11a4b6a913ef0ffd
kevinjameshilton <kjhilton01@gmail.com>
2022-10-28 15:14:31 -0700

    bs4 dependency and init extract_next_links func

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,12 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    
+    if resp.status == 200:
+        soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+        return [link.get('href') for link in soup.find_all('a')]
+    else:
+        return []
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
