a015efec30bf0f332fae3bc6b3f1ad6e3ec87483
Jose Cisneros <jantcisneros@gmail.com>
2022-10-28 17:15:08 -0700

    config test.

--- launch.py
+++ launch.py
@@ -10,6 +10,7 @@ def main(config_file, restart):
     cparser = ConfigParser()
     cparser.read(config_file)
     config = Config(cparser)
+    print(config)
     config.cache_server = get_cache_server(config, restart)
     crawler = Crawler(config, restart)
     crawler.start()
