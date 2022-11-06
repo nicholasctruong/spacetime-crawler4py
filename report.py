9c8a148839622e1d4988d7ae34fbc236b92c3ba4
Jose Cisneros <jantcisneros@gmail.com>
2022-11-05 19:56:35 -0700

    bug fixes.

--- report.py
+++ report.py
@@ -6,8 +6,8 @@ from sys import argv
 
 SHELVED_FILES = [
     'frontier.shelve',
-    'tokens.shelve',
     'word_count.shelve',
+    'tokens.shelve',
     'subdomains.shelve'
 ]
 
@@ -19,7 +19,7 @@ def generate_report() -> None:
     print("Part 1: ", end="")
     if _shelve_file_exists(SHELVED_FILES[0]):
         frontier = shelve.open(SHELVED_FILES[0])
-        print(f"VISITED {len(frontier)} UNIQUE SITES")
+        print(f"VISITED {len(frontier)} UNIQUE SITES\n")
     else:
         print("FRONTIER NOT FOUND")
 
@@ -27,7 +27,7 @@ def generate_report() -> None:
     if _shelve_file_exists(SHELVED_FILES[1]):
         word_count = shelve.open(SHELVED_FILES[1])
         max_words = max(word_count.values(), key=lambda p: p[1])
-        print(f"Site with Highest Word Count: {max_words[0]} | {max_words[1]} words")
+        print(f"Site with Highest Word Count:\n{max_words[0]} | {max_words[1]} words\n")
     else:
         print("WORD_COUNT NOT FOUND")
     
@@ -36,10 +36,10 @@ def generate_report() -> None:
         tokens = shelve.open(SHELVED_FILES[2])
         top_tokens = sorted(tokens.items(), key=lambda w: (-w[1], w[0]))[:50]
         print(
-            '\n' +
-            [
-                f"{word}\t{count}" for word, count in top_tokens
-            ].join('\n')
+            'Top 50 Words\n' +
+            '\n'.join([
+                f"{word:20}{count}" for word, count in top_tokens
+            ]) + '\n'
         )
     else:
         print("TOKENS NOT FOUND")
@@ -48,7 +48,7 @@ def generate_report() -> None:
     if _shelve_file_exists(SHELVED_FILES[3]):
         subdomains = shelve.open(SHELVED_FILES[3])
         max_subdomain = max(subdomains.values(), key=lambda p: p[1])
-        print(f"Most common subdomain: {max_subdomain[0]} | {max_subdomain[1]} times")
+        print(f"Most common subdomain: \n{max_subdomain[0]} | {max_subdomain[1]} times\n")
     else:
         print("SUBDOMAINS NOT FOUND")
 
