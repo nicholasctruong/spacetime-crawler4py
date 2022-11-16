0afbf5183620a43f451670d7e957df1f6ad26968
Kevin James Hilton <kjhilton01@gmail.com>
2022-11-15 22:03:29 -0800

    don't crash when handling connection error

--- crawler/worker.py
+++ crawler/worker.py
@@ -30,35 +30,38 @@ class Worker(Thread):
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
 
-            resp = download(tbd_url, self.config, self.logger)
-            document_size, tokens = scraper.token_info(tbd_url, resp)
+            try:
+                resp = download(tbd_url, self.config, self.logger)
+                document_size, tokens = scraper.token_info(tbd_url, resp)
 
-            self.frontier.add_page_details(
-                    tbd_url,
-                    document_size,
-                    tokens
-            )
+                self.frontier.add_page_details(
+                        tbd_url,
+                        document_size,
+                        tokens
+                )
 
-            self.logger.info(
-                f"Downloaded {tbd_url}, status <{resp.status}>, "
-                f"using cache {self.config.cache_server}.")
+                self.logger.info(
+                    f"Downloaded {tbd_url}, status <{resp.status}>, "
+                    f"using cache {self.config.cache_server}.")
 
-            # url = normalize(tbd_url)
-            # subdomain_urlhash = get_urlhash(url)
-            # if subdomain_urlhash != self.current_subdomain:
-            #     self.current_subdomain = subdomain_urlhash
-            #     self.current_subdomain_time = time.time()
-            # if (time.time() - self.current_subdomain_time >= 600) or self.simhash_index.get_near_dups(Simhash(tokens)):
-            #     self.traps.add(subdomain_urlhash)
-            # if subdomain_urlhash not in self.traps:
-            #     scraped_urls = scraper.scraper(tbd_url, resp)
-            # else:
-            #     scraped_urls = []
+                # url = normalize(tbd_url)
+                # subdomain_urlhash = get_urlhash(url)
+                # if subdomain_urlhash != self.current_subdomain:
+                #     self.current_subdomain = subdomain_urlhash
+                #     self.current_subdomain_time = time.time()
+                # if (time.time() - self.current_subdomain_time >= 600) or self.simhash_index.get_near_dups(Simhash(tokens)):
+                #     self.traps.add(subdomain_urlhash)
+                # if subdomain_urlhash not in self.traps:
+                #     scraped_urls = scraper.scraper(tbd_url, resp)
+                # else:
+                #     scraped_urls = []
 
-            # self.simhash_index.add(str(uuid.uuid4), Simhash(tokens))
+                # self.simhash_index.add(str(uuid.uuid4), Simhash(tokens))
 
-            scraped_urls = scraper.scraper(tbd_url, resp)
-            for scraped_url in scraped_urls:
-                self.frontier.add_url(scraped_url)
-            self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+                scraped_urls = scraper.scraper(tbd_url, resp)
+                for scraped_url in scraped_urls:
+                    self.frontier.add_url(scraped_url)
+                self.frontier.mark_url_complete(tbd_url)
+                time.sleep(self.config.time_delay)
+            except ConnectionError as e:
+                self.logger.info(f"Coudln't download {tbd_url},  {e}")
